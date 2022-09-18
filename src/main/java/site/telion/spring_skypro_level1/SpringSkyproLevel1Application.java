11aaaa6a4f1205741ee006c9f006dc83290fe2e2
Telion <www.neon@mail.ru>
2022-09-18 12:46:22 +0500

    Initial commit

---
+++ src/main/java/site/telion/spring_skypro_level1/SpringSkyproLevel1Application.java
@@ -0,0 +1,13 @@
+package site.telion.spring_skypro_level1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSkyproLevel1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSkyproLevel1Application.class, args);
+    }
+
+}
